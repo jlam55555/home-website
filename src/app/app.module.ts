3a3ba290139c5d191dbb2783c1d94e7a1d0523e6
Jonathan Lam <jlam55555@gmail.com>
2018-04-02 10:51:42 -0400

    added routing to website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CookbookComponent } from './cookbook/cookbook.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CookbookComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
