9f292cf6cf2d6ebb0c1586ca4b71f3d0b9564ca7
Jonathan Lam <jlam55555@gmail.com>
2018-04-06 20:55:31 -0400

    update person to use data from people.ts

---
+++ src/app/person/people.ts
@@ -0,0 +1,6 @@
+export default {
+  jonathan: {
+    name: 'Jonathan Lam',
+    description: 'Ambition teen mathematician and hacker.'
+  }
+}
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageService } from '../page.service';
+import people from './people';
 
 @Component({
   selector: 'app-person',
@@ -11,13 +12,13 @@ export class PersonComponent implements OnInit {
 
   constructor(private pageService: PageService, private activatedRoute: ActivatedRoute) { }
 
-  public name: String;
+  public person: any;
 
   ngOnInit() {
     this.pageService.setPage('person');
 
     // get name
-    this.activatedRoute.params.subscribe(params => this.name = params.name);
+    this.activatedRoute.params.subscribe(params => this.person = people[params.name]);
   }
 
 }
